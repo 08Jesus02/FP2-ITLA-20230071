8f613365c74f5bf63a716a74b45d5d71f461c2f6
Angel J. Brito <angeldejesus.bg2005@gmail.com>
2023-09-23 20:48:25 -0400

    voseto en la interfaz grafica

--- App de Loteria/Form1.Designer.cs
+++ App de Loteria/Form1.Designer.cs
@@ -28,18 +28,102 @@
         /// </summary>
         private void InitializeComponent()
         {
+            textBox1 = new TextBox();
+            button1 = new Button();
+            textBox2 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
+            textBox8 = new TextBox();
+            textBox9 = new TextBox();
             SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(26, 52);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(190, 136);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 1;
+            button1.Text = "JUGAR!";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(165, 230);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 2;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(26, 230);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(100, 23);
+            textBox5.TabIndex = 5;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(307, 230);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(100, 23);
+            textBox6.TabIndex = 6;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(358, 12);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(100, 23);
+            textBox7.TabIndex = 7;
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(307, 52);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(100, 23);
+            textBox8.TabIndex = 8;
+            // 
+            // textBox9
+            // 
+            textBox9.Location = new Point(165, 52);
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(100, 23);
+            textBox9.TabIndex = 9;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(470, 450);
+            Controls.Add(textBox9);
+            Controls.Add(textBox8);
+            Controls.Add(textBox7);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private Button button1;
+        private TextBox textBox2;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private TextBox textBox7;
+        private TextBox textBox8;
+        private TextBox textBox9;
     }
 }
