78451abac7bbac7308bdc7ea3e9d2de459218109
Angel J. Brito <angeldejesus.bg2005@gmail.com>
2023-09-29 04:53:47 -0400

    App hecha

--- App de Loteria/Form1.Designer.cs
+++ App de Loteria/Form1.Designer.cs
@@ -28,45 +28,53 @@
         /// </summary>
         private void InitializeComponent()
         {
-            textBoxApostado = new TextBox();
-            textBox3 = new TextBox();
+            txtBoxApostado = new TextBox();
+            txtBoxGanado = new TextBox();
             label1 = new Label();
             label3 = new Label();
-            textBox4 = new TextBox();
             label6 = new Label();
             label2 = new Label();
             label7 = new Label();
-            textBox2 = new TextBox();
             label8 = new Label();
-            textBox5 = new TextBox();
             textBox1 = new TextBox();
-            textBox7 = new TextBox();
-            textBox6 = new TextBox();
-            label5 = new Label();
+            textBox2 = new TextBox();
             label9 = new Label();
             label4 = new Label();
             label11 = new Label();
             label10 = new Label();
-            button1 = new Button();
+            btnPlay = new Button();
+            groupBox1 = new GroupBox();
+            rdbtnPale = new RadioButton();
+            rdbtnPaleDoble = new RadioButton();
+            label5 = new Label();
+            textBox3 = new TextBox();
+            labelResultado3 = new Label();
+            labelResultado2 = new Label();
+            labelResultado1 = new Label();
+            groupBox1.SuspendLayout();
             SuspendLayout();
             // 
-            // textBoxApostado
+            // txtBoxApostado
             // 
-            textBoxApostado.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
-            textBoxApostado.Location = new Point(289, 27);
-            textBoxApostado.Multiline = true;
-            textBoxApostado.Name = "textBoxApostado";
-            textBoxApostado.Size = new Size(104, 38);
-            textBoxApostado.TabIndex = 7;
+            txtBoxApostado.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            txtBoxApostado.Location = new Point(289, 27);
+            txtBoxApostado.Multiline = true;
+            txtBoxApostado.Name = "txtBoxApostado";
+            txtBoxApostado.Size = new Size(104, 38);
+            txtBoxApostado.TabIndex = 7;
+            txtBoxApostado.Text = "0";
+            txtBoxApostado.TextAlign = HorizontalAlignment.Right;
             // 
-            // textBox3
+            // txtBoxGanado
             // 
-            textBox3.Font = new Font("Segoe UI", 21.75F, FontStyle.Regular, GraphicsUnit.Point);
-            textBox3.Location = new Point(150, 368);
-            textBox3.Multiline = true;
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(142, 54);
-            textBox3.TabIndex = 10;
+            txtBoxGanado.Font = new Font("Segoe UI", 21.75F, FontStyle.Regular, GraphicsUnit.Point);
+            txtBoxGanado.Location = new Point(150, 408);
+            txtBoxGanado.Multiline = true;
+            txtBoxGanado.Name = "txtBoxGanado";
+            txtBoxGanado.Size = new Size(142, 54);
+            txtBoxGanado.TabIndex = 10;
+            txtBoxGanado.Text = "0";
+            txtBoxGanado.TextAlign = HorizontalAlignment.Right;
             // 
             // label1
             // 
@@ -82,29 +90,17 @@
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            label3.Location = new Point(180, 344);
+            label3.Location = new Point(180, 384);
             label3.Name = "label3";
             label3.Size = new Size(66, 21);
             label3.TabIndex = 13;
             label3.Text = "Ganado";
             // 
-            // textBox4
-            // 
-            textBox4.Font = new Font("Segoe UI", 27.75F, FontStyle.Regular, GraphicsUnit.Point);
-            textBox4.Location = new Point(173, 264);
-            textBox4.MaxLength = 2;
-            textBox4.Multiline = true;
-            textBox4.Name = "textBox4";
-            textBox4.Size = new Size(75, 60);
-            textBox4.TabIndex = 17;
-            textBox4.Text = "00";
-            textBox4.TextAlign = HorizontalAlignment.Center;
-            // 
             // label6
             // 
             label6.AutoSize = true;
             label6.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
-            label6.Location = new Point(173, 241);
+            label6.Location = new Point(173, 281);
             label6.Name = "label6";
             label6.Size = new Size(78, 20);
             label6.TabIndex = 21;
@@ -124,51 +120,26 @@
             // 
             label7.AutoSize = true;
             label7.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
-            label7.Location = new Point(26, 241);
+            label7.Location = new Point(26, 281);
             label7.Name = "label7";
             label7.Size = new Size(78, 20);
             label7.TabIndex = 24;
             label7.Text = "Random 3";
             // 
-            // textBox2
-            // 
-            textBox2.Font = new Font("Segoe UI", 27.75F, FontStyle.Regular, GraphicsUnit.Point);
-            textBox2.Location = new Point(26, 264);
-            textBox2.MaxLength = 2;
-            textBox2.Multiline = true;
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(75, 60);
-            textBox2.TabIndex = 23;
-            textBox2.Text = "00";
-            textBox2.TextAlign = HorizontalAlignment.Center;
-            textBox2.TextChanged += textBox2_TextChanged;
-            // 
             // label8
             // 
             label8.AutoSize = true;
             label8.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
-            label8.Location = new Point(318, 241);
+            label8.Location = new Point(318, 281);
             label8.Name = "label8";
             label8.Size = new Size(76, 20);
             label8.TabIndex = 26;
             label8.Text = "Random 1";
             // 
-            // textBox5
-            // 
-            textBox5.Font = new Font("Segoe UI", 27.75F, FontStyle.Regular, GraphicsUnit.Point);
-            textBox5.Location = new Point(318, 264);
-            textBox5.MaxLength = 2;
-            textBox5.Multiline = true;
-            textBox5.Name = "textBox5";
-            textBox5.Size = new Size(75, 60);
-            textBox5.TabIndex = 25;
-            textBox5.Text = "00";
-            textBox5.TextAlign = HorizontalAlignment.Center;
-            // 
             // textBox1
             // 
             textBox1.Font = new Font("Segoe UI", 27.75F, FontStyle.Regular, GraphicsUnit.Point);
-            textBox1.Location = new Point(318, 103);
+            textBox1.Location = new Point(289, 143);
             textBox1.MaxLength = 2;
             textBox1.Multiline = true;
             textBox1.Name = "textBox1";
@@ -177,45 +148,23 @@
             textBox1.Text = "00";
             textBox1.TextAlign = HorizontalAlignment.Center;
             // 
-            // textBox7
+            // textBox2
             // 
-            textBox7.Font = new Font("Segoe UI", 27.75F, FontStyle.Regular, GraphicsUnit.Point);
-            textBox7.Location = new Point(26, 103);
-            textBox7.MaxLength = 2;
-            textBox7.Multiline = true;
-            textBox7.Name = "textBox7";
-            textBox7.Size = new Size(75, 60);
-            textBox7.TabIndex = 29;
-            textBox7.Text = "00";
-            textBox7.TextAlign = HorizontalAlignment.Center;
-            // 
-            // textBox6
-            // 
-            textBox6.Font = new Font("Segoe UI", 27.75F, FontStyle.Regular, GraphicsUnit.Point);
-            textBox6.Location = new Point(173, 103);
-            textBox6.MaxLength = 2;
-            textBox6.Multiline = true;
-            textBox6.Name = "textBox6";
-            textBox6.Size = new Size(75, 60);
-            textBox6.TabIndex = 31;
-            textBox6.Text = "00";
-            textBox6.TextAlign = HorizontalAlignment.Center;
-            // 
-            // label5
-            // 
-            label5.AutoSize = true;
-            label5.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
-            label5.Location = new Point(54, 80);
-            label5.Name = "label5";
-            label5.Size = new Size(17, 20);
-            label5.TabIndex = 30;
-            label5.Text = "3";
+            textBox2.Font = new Font("Segoe UI", 27.75F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox2.Location = new Point(134, 143);
+            textBox2.MaxLength = 2;
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(75, 60);
+            textBox2.TabIndex = 31;
+            textBox2.Text = "00";
+            textBox2.TextAlign = HorizontalAlignment.Center;
             // 
             // label9
             // 
             label9.AutoSize = true;
             label9.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
-            label9.Location = new Point(201, 80);
+            label9.Location = new Point(161, 120);
             label9.Name = "label9";
             label9.Size = new Size(17, 20);
             label9.TabIndex = 32;
@@ -225,7 +174,7 @@
             // 
             label4.AutoSize = true;
             label4.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
-            label4.Location = new Point(346, 80);
+            label4.Location = new Point(317, 120);
             label4.Name = "label4";
             label4.Size = new Size(15, 20);
             label4.TabIndex = 28;
@@ -246,73 +195,165 @@
             // 
             label10.AutoSize = true;
             label10.Font = new Font("Segoe UI", 21.75F, FontStyle.Bold, GraphicsUnit.Point);
-            label10.Location = new Point(113, 375);
+            label10.Location = new Point(113, 415);
             label10.Name = "label10";
             label10.Size = new Size(34, 40);
             label10.TabIndex = 35;
             label10.Text = "$";
             // 
-            // button1
+            // btnPlay
             // 
-            button1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            button1.Location = new Point(173, 181);
-            button1.Name = "button1";
-            button1.Size = new Size(75, 40);
-            button1.TabIndex = 36;
-            button1.Text = "JUGAR!";
-            button1.UseVisualStyleBackColor = true;
+            btnPlay.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            btnPlay.Location = new Point(173, 221);
+            btnPlay.Name = "btnPlay";
+            btnPlay.Size = new Size(75, 40);
+            btnPlay.TabIndex = 36;
+            btnPlay.Text = "JUGAR!";
+            btnPlay.UseVisualStyleBackColor = true;
+            btnPlay.Click += btnPlay_Click;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(rdbtnPale);
+            groupBox1.Controls.Add(rdbtnPaleDoble);
+            groupBox1.Location = new Point(48, 82);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(326, 35);
+            groupBox1.TabIndex = 37;
+            groupBox1.TabStop = false;
+            // 
+            // rdbtnPale
+            // 
+            rdbtnPale.AutoSize = true;
+            rdbtnPale.Checked = true;
+            rdbtnPale.Location = new Point(253, 10);
+            rdbtnPale.Name = "rdbtnPale";
+            rdbtnPale.Size = new Size(47, 19);
+            rdbtnPale.TabIndex = 1;
+            rdbtnPale.TabStop = true;
+            rdbtnPale.Text = "Pale";
+            rdbtnPale.UseVisualStyleBackColor = true;
+            // 
+            // rdbtnPaleDoble
+            // 
+            rdbtnPaleDoble.AutoSize = true;
+            rdbtnPaleDoble.Location = new Point(36, 11);
+            rdbtnPaleDoble.Name = "rdbtnPaleDoble";
+            rdbtnPaleDoble.Size = new Size(81, 19);
+            rdbtnPaleDoble.TabIndex = 0;
+            rdbtnPaleDoble.TabStop = true;
+            rdbtnPaleDoble.Text = "Doble Pale";
+            rdbtnPaleDoble.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Location = new Point(53, 120);
+            label5.Name = "label5";
+            label5.Size = new Size(17, 20);
+            label5.TabIndex = 39;
+            label5.Text = "3";
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Segoe UI", 27.75F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox3.Location = new Point(26, 143);
+            textBox3.MaxLength = 2;
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(75, 60);
+            textBox3.TabIndex = 38;
+            textBox3.Text = "00";
+            textBox3.TextAlign = HorizontalAlignment.Center;
+            // 
+            // labelResultado3
+            // 
+            labelResultado3.AutoSize = true;
+            labelResultado3.Font = new Font("Segoe UI", 26.25F, FontStyle.Regular, GraphicsUnit.Point);
+            labelResultado3.Location = new Point(33, 317);
+            labelResultado3.Name = "labelResultado3";
+            labelResultado3.Size = new Size(58, 47);
+            labelResultado3.TabIndex = 41;
+            labelResultado3.Text = "00";
+            // 
+            // labelResultado2
+            // 
+            labelResultado2.AutoSize = true;
+            labelResultado2.Font = new Font("Segoe UI", 26.25F, FontStyle.Regular, GraphicsUnit.Point);
+            labelResultado2.Location = new Point(183, 317);
+            labelResultado2.Name = "labelResultado2";
+            labelResultado2.Size = new Size(58, 47);
+            labelResultado2.TabIndex = 44;
+            labelResultado2.Text = "00";
+            // 
+            // labelResultado1
+            // 
+            labelResultado1.AutoSize = true;
+            labelResultado1.Font = new Font("Segoe UI", 26.25F, FontStyle.Regular, GraphicsUnit.Point);
+            labelResultado1.Location = new Point(325, 317);
+            labelResultado1.Name = "labelResultado1";
+            labelResultado1.Size = new Size(58, 47);
+            labelResultado1.TabIndex = 45;
+            labelResultado1.Text = "00";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(422, 450);
-            Controls.Add(button1);
+            ClientSize = new Size(426, 490);
+            Controls.Add(labelResultado1);
+            Controls.Add(labelResultado2);
+            Controls.Add(labelResultado3);
+            Controls.Add(label5);
+            Controls.Add(textBox3);
+            Controls.Add(groupBox1);
+            Controls.Add(btnPlay);
             Controls.Add(label10);
             Controls.Add(label11);
             Controls.Add(label9);
-            Controls.Add(textBox6);
-            Controls.Add(label5);
-            Controls.Add(textBox7);
+            Controls.Add(textBox2);
             Controls.Add(label4);
             Controls.Add(textBox1);
             Controls.Add(label8);
-            Controls.Add(textBox5);
             Controls.Add(label7);
-            Controls.Add(textBox2);
             Controls.Add(label2);
             Controls.Add(label6);
-            Controls.Add(textBox4);
             Controls.Add(label3);
             Controls.Add(label1);
-            Controls.Add(textBox3);
-            Controls.Add(textBoxApostado);
+            Controls.Add(txtBoxGanado);
+            Controls.Add(txtBoxApostado);
             Name = "Form1";
             Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
-        private TextBox textBoxApostado;
-        private TextBox textBox3;
+        private TextBox txtBoxApostado;
+        private TextBox txtBoxGanado;
         private Label label1;
         private Label label3;
-        private TextBox textBox4;
         private Label label6;
         private Label label2;
         private Label label7;
-        private TextBox textBox2;
         private Label label8;
-        private TextBox textBox5;
         private TextBox textBox1;
-        private TextBox textBox7;
-        private TextBox textBox6;
-        private Label label5;
+        private TextBox textBox2;
         private Label label9;
         private Label label4;
         private Label label11;
         private Label label10;
-        private Button button1;
+        private Button btnPlay;
+        private GroupBox groupBox1;
+        private RadioButton rdbtnPale;
+        private RadioButton rdbtnPaleDoble;
+        private Label label5;
+        private TextBox textBox3;
+        private Label labelResultado3;
+        private Label labelResultado2;
+        private Label labelResultado1;
     }
 }
--- App de Loteria/Form1.cs
+++ App de Loteria/Form1.cs
@@ -1,15 +1,119 @@
+using System;
+using System.Windows.Forms;
+
 namespace App_de_Loteria
 {
     public partial class Form1 : Form
     {
+        private Random random = new Random();
+
+        double Numero1, Numero2, Numero3, Apostado, Ganado;
+        private const double Primera = 100;
+        private const double Segunda = 50;
+        private const double Tercera = 10;
+
+        double Random1, Random2, Random3;
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void textBox2_TextChanged(object sender, EventArgs e)
+        public double GenerarNumeroAleatorio()
         {
+            return random.Next(100);
+        }
 
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            if (EsNumeroValido(textBox1.Text) && EsNumeroValido(textBox2.Text) && EsNumeroValido(textBox3.Text) && EsNumeroValido(txtBoxApostado.Text) && EsNumeroValido(txtBoxGanado.Text))
+            {
+                Numero1 = Convert.ToDouble(textBox1.Text);
+                Numero2 = Convert.ToDouble(textBox2.Text);
+                Numero3 = Convert.ToDouble(textBox3.Text);
+                Apostado = Convert.ToDouble(txtBoxApostado.Text);
+                Ganado = Convert.ToDouble(txtBoxGanado.Text);
+
+                Random1 = GenerarNumeroAleatorio();
+                Random2 = GenerarNumeroAleatorio();
+                Random3 = GenerarNumeroAleatorio();
+
+                labelResultado1.Text = Random1.ToString();
+                labelResultado2.Text = Random2.ToString();
+                labelResultado3.Text = Random3.ToString();
+
+                if (rdbtnPale.Checked == true)
+                {
+                    if (Numero1 == Random1)
+                    {
+                        Apostado *= Primera;
+                        Ganado = Apostado;
+                        txtBoxGanado.Text = Ganado.ToString();
+                    }
+                    else if (Numero1 == Random2)
+                    {
+                        Apostado *= Segunda;
+                        Ganado = Apostado;
+                        txtBoxGanado.Text = Ganado.ToString();
+                    }
+                    else if (Numero1 == Random3)
+                    {
+                        Apostado *= Tercera;
+                        Ganado = Apostado;
+                        txtBoxGanado.Text = Ganado.ToString();
+                    }
+                    else if (Random1 != Numero1)
+                    {
+                        txtBoxGanado.Text = "0";
+                    }
+
+
+                }
+                else if (rdbtnPaleDoble.Checked == true)
+                {
+                    if (Numero2 == Random2 || Numero3 == Random3)
+                    {
+                        Apostado *= Primera;
+                        Ganado = Apostado;
+                        txtBoxGanado.Text = Ganado.ToString();
+                    }
+                    else if (Numero2 == Random3 || Numero3 == Random2)
+                    {
+                        Apostado *= Segunda;
+                        Ganado = Apostado;
+                        txtBoxGanado.Text = Ganado.ToString();
+                    }
+                    else if (Numero2 == Random1 || Numero3 == Random1) //aqui seria como si Random1 fuera el Random3 para seguir con el orden de que lo apostado se multiplica segun que tan cerca de donde apostaste salga el numero */
+                    {   
+                        Apostado *= Tercera;
+                        Ganado = Apostado;
+                        txtBoxGanado.Text = Ganado.ToString();
+                    }
+                    else if (Numero2 != Random1 || Numero2 != Random2 || Numero2 != Random3) 
+                    {
+                        txtBoxGanado.Text = "0";
+                    }
+
+
+
+                }
+            }
+        }
+
+        // Función para verificar si una cadena puede convertirse en número double
+        private bool EsNumeroValido(string valor)
+        {
+            if (string.IsNullOrWhiteSpace(valor))
+            {
+                return false;
+            }
+
+            if (double.TryParse(valor, out _))
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
