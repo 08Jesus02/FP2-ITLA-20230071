fa131bb1b9b0050a649d59aa27fae8f4d4e538ec
Angel J. Brito <angeldejesus.bg2005@gmail.com>
2023-10-15 00:08:00 -0400

    XMLLinq Ejercicio en porceso

---
+++ XMLLinqEjercicio/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace XMLLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var filename = @"C:\Users\Angel Brito\source\repos\FP2-ITLA-20230071\XMLLinqEjercicio\store.xml";
+
+            XElement storeXML = XElement.Load(filename);
+
+            var beers = from e in storeXML.Element("beers").Elements("beer")
+                         select e;
+
+            foreach (var beer in beers)
+            {
+                Console.WriteLine(beer.Value);
+            }
+        }
+    }
+}
