12c1d791b7a65725d9777a2f6dbc40d25ab06321
Angel J. Brito <angeldejesus.bg2005@gmail.com>
2023-10-16 00:13:59 -0400

    Ajustes

---
+++ XML-Ejercicios/XMLLinqEjercicio/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace XMLLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var filename = @"/Users/angelj.soriano/Todo ITLA/3/Fundamentos de Programacion 2/FP2-ITLA-20230071/XML-Ejercicios/XMLLinqEjercicio/store.xml";
+
+            XElement storeXML = XElement.Load(filename);
+
+            var beers = from e in storeXML.Element("beers").Elements("beer")
+                        select e;
+
+            foreach (var beer in beers)
+            {
+                Console.WriteLine(beer.Value);
+            }
+        }
+    }
+}
