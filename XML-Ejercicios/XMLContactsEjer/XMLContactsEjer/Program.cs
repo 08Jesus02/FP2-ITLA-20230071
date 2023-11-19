9c0fdb20bba642611b2583255243cb93f27623bc
angelj.brito <20230071@itla.edu.do>
2023-11-19 13:13:49 -0400

    XmlContacs en proceso

--- XML-Ejercicios/XMLContactsEjer/XMLContactsEjer/Program.cs
+++ XML-Ejercicios/XMLContactsEjer/XMLContactsEjer/Program.cs
@@ -6,5 +6,33 @@ using System.Xml;
 
 namespace XMLContacs
 {
-    p
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UsingXMLWriter();
+        }
+
+        private static void UsingXMLWriter()
+        {
+            XmlWriter xmlWriter = xmlWriter.Create("UsingXmlWriter.xml");
+
+            xmlWriter.WriteStartDocument();
+
+            xmlWriter.WriteStartElement("Contacts");
+
+            xmlWriter.WriteStartElement("Contact");
+            xmlWriter.WriteAttributeString("Phone", "8298412717");
+            xmlWriter.WriteString("El Transportador");
+            xmlWriter.WriteEndElement();
+
+            xmlWriter.WriteStartElement("Contact");
+            xmlWriter.WriteAttributeString("Phone", "8097817346");
+            xmlWriter.WhiteAttributeString("WorkPhone", "8095789235");
+            xmlWriter.WriterString("Toretto");
+
+            xmlWriter.WriteEndDocument();
+            xmlWriter.Close();
+        }
+    }
 }
