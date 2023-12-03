acb888dce26066b656971fc2f0555fa8da3e2178
Angel J. Brito <angeldejesus.bg2005@gmail.com>
2023-12-03 02:45:27 -0400

    Juego de esferas y litedb en proceso

---
+++ juegoEsferasDragon/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace juegoEsferasDragon
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            pictureBox1 = new PictureBox();
+            lblPuntaje = new Label();
+            timer1 = new System.Windows.Forms.Timer(components);
+            timer2 = new System.Windows.Forms.Timer(components);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(12, 492);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(331, 58);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // lblPuntaje
+            // 
+            lblPuntaje.AutoSize = true;
+            lblPuntaje.BackColor = Color.Transparent;
+            lblPuntaje.Font = new Font("Impact", 26.25F, FontStyle.Bold, GraphicsUnit.Point);
+            lblPuntaje.ForeColor = Color.Black;
+            lblPuntaje.Location = new Point(3, 6);
+            lblPuntaje.Name = "lblPuntaje";
+            lblPuntaje.Size = new Size(39, 43);
+            lblPuntaje.TabIndex = 1;
+            lblPuntaje.Text = "0";
+            // 
+            // timer1
+            // 
+            timer1.Tick += timer1_Tick;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(355, 559);
+            Controls.Add(pictureBox1);
+            Controls.Add(lblPuntaje);
+            Name = "Form1";
+            Text = "Juego Dragon Ball";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Label lblPuntaje;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
---
+++ juegoEsferasDragon/Form1.cs
@@ -0,0 +1,14 @@
+namespace juegoEsferasDragon
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
