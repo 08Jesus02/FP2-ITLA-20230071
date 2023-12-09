0495613d44d60190d0246ed1d08bd60d3a3dffba
08Jesus02 <129822817+08Jesus02@users.noreply.github.com>
2023-12-08 22:05:43 -0400

    ajustes

---
+++ XML-Ejercicios/ConcecionarioEjer/ConcecionarioEjer/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Xml;
+
+namespace XmlConcesionario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load("Concesionario.xml");
+
+            foreach (XmlNode n1 in doc.DocumentElement.ChildNodes)
+            {
+                if (n1.HasChildNodes)
+                {
+                    foreach (XmlNode n2 in n1.ChildNodes)
+                    {
+                        String matricula = n2.Attributes["matricula"].Value;
+                        Console.WriteLine("Matricula: " + matricula);
+
+                        foreach (XmlNode n3 in n2.ChildNodes)
+                        {
+                            Console.WriteLine(n3.Name + " " + n3.InnerText);
+                        }
+                    }
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
