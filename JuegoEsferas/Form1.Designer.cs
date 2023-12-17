dda3ef77ac4af28e1805517393cb8ab3aafb908e
Angel De Jesus Brito Garcia <20230071@itla.edu.do>
2023-12-17 00:45:32 -0400

    Ajustes

---
+++ JuegoEsferas/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace JuegoEsferas
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            pictureBox1 = new PictureBox();
+            lblPuntaje = new Label();
+            timer1 = new System.Windows.Forms.Timer(components);
+            timer2 = new System.Windows.Forms.Timer(components);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.bar2;
+            pictureBox1.Location = new Point(12, 379);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(304, 50);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // lblPuntaje
+            // 
+            lblPuntaje.AutoSize = true;
+            lblPuntaje.BackColor = Color.Transparent;
+            lblPuntaje.Cursor = Cursors.SizeNESW;
+            lblPuntaje.FlatStyle = FlatStyle.Flat;
+            lblPuntaje.Font = new Font("Impact", 21.75F, FontStyle.Bold, GraphicsUnit.Point);
+            lblPuntaje.Location = new Point(12, 9);
+            lblPuntaje.Name = "lblPuntaje";
+            lblPuntaje.Size = new Size(32, 36);
+            lblPuntaje.TabIndex = 1;
+            lblPuntaje.Text = "0";
+            lblPuntaje.Click += label1_Click;
+            // 
+            // timer1
+            // 
+            timer1.Enabled = true;
+            timer1.Interval = 1;
+            // 
+            // timer2
+            // 
+            timer2.Enabled = true;
+            timer2.Interval = 1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.fondo;
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(328, 441);
+            Controls.Add(lblPuntaje);
+            Controls.Add(pictureBox1);
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            Name = "Form1";
+            Text = "Juego de Esferas";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Label lblPuntaje;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
---
+++ JuegoEsferas/Form1.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Media;
+using System.Windows.Forms;
+
+namespace JuegoEsferas
+{
+    public partial class Form1 : Form
+    {
+        List<PictureBox> Lista = new List<PictureBox>();
+        int Velocidad = 1, ColorPong = 1;
+        Random ColorBalon = new Random();
+        SoundPlayer Sonido = new SoundPlayer();
+
+        public Form1()
+        {
+            InitializeComponent();
+            IniciarJuego();
+        }
+
+        public void IniciarJuego()
+        {
+            Velocidad = 1;
+            pictureBox1.Tag = 1;
+            this.KeyPreview = true;
+            lblPuntaje.Text = "0";
+            crearPelota(Lista, this);
+            timer1.Start();
+            timer2.Start();
+            Sonido.Stream = Properties.Resources.bin;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < Lista.Count; i++)
+            {
+                int MovimientoY = Lista[i].Location.Y + Velocidad;
+                Lista[i].Location = new Point(Lista[i].Location.X, MovimientoY);
+
+                // Puedes agregar una impresión en la consola para verificar la posición
+                Console.WriteLine($"Pelota {i}: X={Lista[i].Location.X}, Y={Lista[i].Location.Y}");
+            }
+        }
+
+        public void crearPelota(List<PictureBox> ListaPelota, Form formulario)
+        {
+            PictureBox pb = new PictureBox();
+            pb.Location = new Point(135, -80);
+            int NumeroColor = ColorBalon.Next(1, 5);
+            pb.Image = (Bitmap)Properties.Resources.ResourceManager.GetObject("ball" + NumeroColor);
+            pb.Tag = NumeroColor;
+            pb.BackColor = Color.Transparent;
+            pb.SizeMode = PictureBoxSizeMode.AutoSize;
+
+            formulario.Controls.Add(pb);
+            ListaPelota.Add(pb);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
