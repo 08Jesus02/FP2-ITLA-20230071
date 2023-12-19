f9f3d1ce780ffa33b5113e20244ea1725669b868
Angel De Jesus Brito Garcia <20230071@itla.edu.do>
2023-12-19 02:37:02 -0400

    Juego Atrapa Monedas Listo

--- Juegos/JuegoAtrapaMonedas/Form1.Designer.cs
+++ Juegos/JuegoAtrapaMonedas/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             panel1 = new Panel();
             panel2 = new Panel();
             panel3 = new Panel();
@@ -54,14 +55,14 @@
             // 
             // panel2
             // 
-            panel2.Location = new Point(314, 234);
+            panel2.Location = new Point(190, 234);
             panel2.Name = "panel2";
             panel2.Size = new Size(111, 101);
             panel2.TabIndex = 1;
             // 
             // panel3
             // 
-            panel3.Location = new Point(192, 234);
+            panel3.Location = new Point(315, 234);
             panel3.Name = "panel3";
             panel3.Size = new Size(111, 101);
             panel3.TabIndex = 2;
@@ -69,35 +70,38 @@
             // pictureBox1
             // 
             pictureBox1.BackColor = Color.Transparent;
-            pictureBox1.Image = Properties.Resources.btnPararVerdeDesactio;
+            pictureBox1.Image = Properties.Resources.btnPararAzulDesactio;
             pictureBox1.Location = new Point(68, 347);
             pictureBox1.Name = "pictureBox1";
             pictureBox1.Size = new Size(100, 50);
             pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox1.TabIndex = 3;
             pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
             // 
             // pictureBox2
             // 
             pictureBox2.BackColor = Color.Transparent;
-            pictureBox2.Image = Properties.Resources.btnPararAzulDesactio;
-            pictureBox2.Location = new Point(318, 347);
+            pictureBox2.Image = Properties.Resources.btnPararRojoDesactio;
+            pictureBox2.Location = new Point(196, 347);
             pictureBox2.Name = "pictureBox2";
             pictureBox2.Size = new Size(100, 50);
             pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox2.TabIndex = 4;
             pictureBox2.TabStop = false;
+            pictureBox2.Click += pictureBox2_Click;
             // 
             // pictureBox3
             // 
             pictureBox3.BackColor = Color.Transparent;
-            pictureBox3.Image = Properties.Resources.btnPararRojoDesactio;
-            pictureBox3.Location = new Point(198, 347);
+            pictureBox3.Image = Properties.Resources.btnPararVerdeDesactio;
+            pictureBox3.Location = new Point(320, 347);
             pictureBox3.Name = "pictureBox3";
             pictureBox3.Size = new Size(100, 50);
             pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox3.TabIndex = 5;
             pictureBox3.TabStop = false;
+            pictureBox3.Click += pictureBox3_Click;
             // 
             // pictureBox4
             // 
@@ -109,21 +113,25 @@
             pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox4.TabIndex = 6;
             pictureBox4.TabStop = false;
+            pictureBox4.Click += pictureBox4_Click;
             // 
             // timer1
             // 
             timer1.Enabled = true;
             timer1.Interval = 1;
+            timer1.Tick += timer1_Tick;
             // 
             // timer2
             // 
             timer2.Enabled = true;
             timer2.Interval = 1;
+            timer2.Tick += timer2_Tick;
             // 
             // timer3
             // 
             timer3.Enabled = true;
             timer3.Interval = 1;
+            timer3.Tick += timer3_Tick;
             // 
             // Form1
             // 
@@ -140,8 +148,10 @@
             Controls.Add(panel2);
             Controls.Add(panel1);
             FormBorderStyle = FormBorderStyle.FixedSingle;
+            Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "Form1";
-            Text = "Form1";
+            Text = "Traga Monedas";
+            Load += Form1_Load;
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
--- Juegos/JuegoAtrapaMonedas/Form1.cs
+++ Juegos/JuegoAtrapaMonedas/Form1.cs
@@ -8,9 +8,9 @@ namespace JuegoAtrapaMonedas
         GrupoFichasTres = new List<PictureBox>();
 
         int MovimientoY = 0, PosicionSiguiente, LongitudFicha, posicionEjeX = 3;
-        bool indicador1 = false, indicador2 = false, indicador3 = false;
+        bool Indicador1 = false, Indicador2 = false, Indicador3 = false;
 
-        String nombreFicha1 = "", nombreFicha2 = "", nombreFicha3 = "";
+        String NombreFicha1 = "", NombreFicha2 = "", NombreFicha3 = "";
         public Form1()
         {
             InitializeComponent();
@@ -21,10 +21,140 @@ namespace JuegoAtrapaMonedas
 
         }
 
-        public List<PictureBox> CrearImagenes(List<PictureBox> Lista, Panel panel, String Nombretag)
+        public List<PictureBox> CrearImagenes(List<PictureBox> Lista, Panel panel, String NombreTag)
         {
+            LongitudFicha = 100;
+            for (int i = 0; i < 10; i++)
+            {
+                PictureBox pb = new PictureBox();
+                pb.Image = (Bitmap)Properties.Resources.ResourceManager.GetObject("btnone" + (i + 1));
+                pb.Size = new Size(LongitudFicha, LongitudFicha);
+                pb.SizeMode = PictureBoxSizeMode.StretchImage;
+                pb.Name = string.Format("{0}", i + NombreTag);
+                pb.Tag = NombreTag + "_" + i;
+                int Mov = pb.Location.Y;
+                pb.Location = new Point(posicionEjeX, i * LongitudFicha);
+                Lista.Add(pb);
+                panel.Controls.Add(Lista[i]);
+
+            }
+            return Lista;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CrearImagenes(GrupoFichasUno, panel1, "Uno");
+            CrearImagenes(GrupoFichasDos, panel2, "Dos");
+            CrearImagenes(GrupoFichasTres, panel3, "Tres");
+        }
+
+        public String GirarFichas(List<PictureBox> Lista, Panel panel, System.Windows.Forms.Timer timer, bool bandera)
+        {
+            String FichaSeleccionada = "";
+
+            int posicionDeDetencion = -10; // Establece la posición de detención deseada
+
+            for (int i = 0; i < Lista.Count; i++)
+            {
+                MovimientoY = Lista[i].Location.Y;
+                Lista[i].Location = new Point(posicionEjeX, MovimientoY - 10);
+
+                if (Lista[i].Location.Y <= -LongitudFicha)
+                {
+                    PosicionSiguiente = Lista[((Lista.Count) - 1)].Location.Y;
+                    Lista[i].Location = new Point(posicionEjeX, PosicionSiguiente + LongitudFicha);
+                    Lista.Add(Lista[i]);
+                    panel.Controls.Add(Lista[i]);
+                    Lista.RemoveAt(i);
+                }
+
+                if (bandera)
+                {
+                    if (Lista[i].Location.Y == posicionDeDetencion)
+                    {
+                        timer.Stop();
+                        FichaSeleccionada = Lista[i].Tag.ToString();
+                    }
+                }
+                else
+                {
+                    FichaSeleccionada = "";
+                }
+            }
+            return FichaSeleccionada;
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            NombreFicha1 = GirarFichas(GrupoFichasUno, panel1, timer1, Indicador1);
 
         }
 
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            Indicador1 = false;
+            Indicador2 = false;
+            Indicador3 = false;
+            timer1.Start();
+            pictureBox4.Image = Properties.Resources.btnStop;
+            pictureBox1.Image = Properties.Resources.btnPararAzul;
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            NombreFicha2 = GirarFichas(GrupoFichasDos, panel2, timer2, Indicador2);
+
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            NombreFicha3 = GirarFichas(GrupoFichasTres, panel3, timer3, Indicador3);
+            if (NombreFicha3 != "")
+            {
+                pictureBox4.Image = Properties.Resources.btnStart;
+
+                if ((NumeroFicha(NombreFicha1) == NumeroFicha(NombreFicha2)) && (NumeroFicha(NombreFicha2) == NumeroFicha(NombreFicha3)))
+                {
+                    MessageBox.Show("Ganaste");
+                }
+
+                else
+                {
+                    MessageBox.Show("Perdiste");
+                }
+            }
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Indicador1 = true;
+            timer2.Start();
+            pictureBox1.Image = Properties.Resources.btnPararAzulDesactio;
+            pictureBox2.Image = Properties.Resources.btnPararRojo;
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            Indicador2 = true;
+            timer3.Start();
+            pictureBox2.Image = Properties.Resources.btnPararRojoDesactio;
+            pictureBox3.Image = Properties.Resources.btnPararVerde;
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            Indicador3 = true;
+            pictureBox3.Image = Properties.Resources.btnPararVerdeDesactio;
+
+
+        }
+
+        public int NumeroFicha(String Nombre)
+        {
+            string[] Posicion = Nombre.Split("_".ToCharArray());
+            return Convert.ToInt32(Posicion[1]);
+        }
     }
 }
